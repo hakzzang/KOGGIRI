aa3ec39680d97ac9043817e2cb870c13bf8ed246
hakzzang <mbh930312@naver.com>
2022-02-01 21:33:03 +0900

    [ui] 위클리 캘린더 기본 콘텐츠 추가

--- app/src/main/java/com/hbs/koggiri/ui/history/CalendarScreen.kt
+++ app/src/main/java/com/hbs/koggiri/ui/history/CalendarScreen.kt
@@ -1,105 +1,23 @@
 package com.hbs.koggiri.ui.history
 
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.lazy.LazyRow
-import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Alignment.Companion.CenterHorizontally
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.unit.dp
-import com.hbs.koggiri.models.CalendarFactory
 import com.hbs.koggiri.models.CalendarPresentation
-import com.hbs.koggiri.ui.component.KoggiriLargeTitle
-import com.hbs.koggiri.ui.component.KoggiriMediumTitle
-import com.hbs.koggiri.ui.component.KoggiriSmallText
-import com.hbs.koggiri.ui.theme.PointBlueColor
-import com.hbs.koggiri.ui.theme.PointRedColor
-import org.intellij.lang.annotations.JdkConstants
-import java.util.*
+import com.hbs.koggiri.ui.component.getCardModifier
+import com.hbs.koggiri.ui.history.component.CalendarContent
 
 @Composable
 fun CalendarScreenBody(
     calendarPresentations: List<CalendarPresentation>,
-    onClickCalendarItem: (CalendarPresentation) -> Unit
-) {
-    val scrollState = rememberScrollState()
-    Box(
-        modifier = Modifier
-            .fillMaxSize()
-            .background(Color.White)
-    ) {
-        CalendarContent(
-            calendarPresentations,
-            onClickCalendarItem
-        )
-    }
-}
-
-@Composable
-fun CalendarContent(
-    calendars: List<CalendarPresentation>,
     onClickCalendarItem: (CalendarPresentation) -> Unit,
     modifier: Modifier = Modifier
 ) {
-    LazyRow(
-        modifier = modifier
-            .wrapContentHeight()
-            .padding(start = 16.dp, end = 16.dp, top = 16.dp)
-    ) {
-        items(calendars) {
-            CalendarItem(it)
-        }
-    }
-}
+    val scrollState = rememberScrollState()
 
-@Composable
-fun CalendarItem(
-    calendar: CalendarPresentation,
-    modifier: Modifier = Modifier
-) {
-    Column(
-        modifier = modifier
-            .width(64.dp)
-            .wrapContentHeight(),
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-//        if (calendar.isSelection) {
-//
-//        } else {
-//
-//        }
-
-        val color =
-            when (calendar.date.dayOfWeek.value) {
-                7 -> {
-                    PointRedColor
-                }
-                6 -> {
-                    PointBlueColor
-                }
-                else -> {
-                    Color.Black
-                }
-            }
-
-        KoggiriSmallText(
-            title = calendar.dayText,
-            color = color,
-            modifier = Modifier
-                .fillMaxWidth()
-                .wrapContentHeight()
-        )
-
-        KoggiriMediumTitle(
-            title = calendar.dayNumber,
-            color = color,
-            modifier = Modifier
-                .fillMaxWidth()
-                .wrapContentHeight()
-        )
-    }
+    CalendarContent(
+        calendars = calendarPresentations,
+        onClickCalendarItem = onClickCalendarItem,
+        modifier = Modifier.getCardModifier()
+    )
 }
---
+++ app/src/main/java/com/hbs/koggiri/ui/history/component/CalendarContents.kt
@@ -0,0 +1,129 @@
+package com.hbs.koggiri.ui.history.component
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Done
+import androidx.compose.material3.Icon
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import com.hbs.koggiri.models.CalendarPresentation
+import com.hbs.koggiri.ui.component.KoggiriMediumTitle
+import com.hbs.koggiri.ui.component.KoggiriSmallText
+import com.hbs.koggiri.ui.home.component.StatItemList
+import com.hbs.koggiri.ui.theme.PointBlueColor
+import com.hbs.koggiri.ui.theme.PointRedColor
+
+@Composable
+fun CalendarContent(
+    calendars: List<CalendarPresentation>,
+    onClickCalendarItem: (CalendarPresentation) -> Unit,
+    modifier: Modifier = Modifier
+) {
+
+    val stats = listOf(
+        "Days Running",
+        "Days Eating Salad",
+        "Days Watching Calmdown Man"
+    )
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.White)
+    ) {
+        CalendarList(
+            calendars,
+            onClickCalendarItem
+        )
+        Column(modifier = modifier) {
+            Row {
+                KoggiriMediumTitle(
+                    title = "Your histories",
+                    modifier = Modifier
+                        .weight(1f)
+                        .wrapContentWidth(Alignment.Start)
+                )
+                Icon(
+                    Icons.Filled.Done,
+                    tint = PointRedColor,
+                    contentDescription = "추가",
+                    modifier = Modifier.size(32.dp)
+                )
+                Spacer(modifier = Modifier.size(12.dp))
+            }
+            StatItemList(stats = stats, onClickStatContent = { })
+        }
+    }
+}
+
+@Composable
+fun CalendarList(
+    calendars: List<CalendarPresentation>,
+    onClickCalendarItem: (CalendarPresentation) -> Unit,
+    modifier: Modifier = Modifier
+) {
+    LazyRow(
+        modifier = modifier
+            .wrapContentHeight()
+            .padding(start = 16.dp, end = 16.dp, top = 16.dp)
+    ) {
+        items(calendars) {
+            CalendarItem(it)
+        }
+    }
+}
+
+
+@Composable
+fun CalendarItem(
+    calendar: CalendarPresentation,
+    modifier: Modifier = Modifier
+) {
+    Column(
+        modifier = modifier
+            .width(64.dp)
+            .wrapContentHeight(),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+//        if (calendar.isSelection) {
+//
+//        } else {
+//
+//        }
+
+        val color =
+            when (calendar.date.dayOfWeek.value) {
+                7 -> {
+                    PointRedColor
+                }
+                6 -> {
+                    PointBlueColor
+                }
+                else -> {
+                    Color.Black
+                }
+            }
+
+        KoggiriSmallText(
+            title = calendar.dayText,
+            color = color,
+            modifier = Modifier
+                .fillMaxWidth()
+                .wrapContentHeight()
+        )
+
+        KoggiriMediumTitle(
+            title = calendar.dayNumber,
+            color = color,
+            modifier = Modifier
+                .fillMaxWidth()
+                .wrapContentHeight()
+        )
+    }
+}
