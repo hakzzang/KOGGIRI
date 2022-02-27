4234e086594113397bdb41e5e5814b54a71d193e
hakzzang <mbh930312@naver.com>
2022-02-27 22:40:56 +0900

    [ui] CardModifier 사이즈 영역 수정

--- app/src/main/java/com/hbs/koggiri/ui/component/KoggiriModifier.kt
+++ app/src/main/java/com/hbs/koggiri/ui/component/KoggiriModifier.kt
@@ -3,6 +3,7 @@ package com.hbs.koggiri.ui.component
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.ui.Modifier
@@ -13,7 +14,7 @@ import com.hbs.koggiri.ui.theme.NeutralVariant95
 
 fun Modifier.getCardModifier(): Modifier = composed {
     this
-        .fillMaxSize()
+        .wrapContentSize()
         .background(NeutralVariant95)
         .padding(start = 16.dp, end = 16.dp, top = 16.dp)
         .clip(RoundedCornerShape(16.dp))
